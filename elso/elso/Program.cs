79ddc98a8c33c23d9a8a8abe979b86c9ad1f02d8
Pintér Csongor <pinterc21@ADY-NAGYATAD.SULINET.HU>
2023-09-07 08:43:56 +0200

    i

--- elso/elso/Program.cs
+++ elso/elso/Program.cs
@@ -21,8 +21,45 @@ namespace elso
             Convert.ToInt32(3);
             Console.WriteLine(10 % 3);
 
+            int a;
+            a = 5;
+            if (a > 2)
+            {
+                Console.WriteLine("Nagyobb");
+            }
+            else if (a < 2)
+            {
+                Console.WriteLine("Kisebb");
+            }
+            else if (a % 2==0) 
+            {
+                Console.WriteLine("Páros");
+            }
+            else 
+            {
+                Console.WriteLine("Egyenlő");
+            }
 
 
+            switch (a) 
+            {
+                case 1:
+                    Console.WriteLine("egy");
+                    break;
+                case 2:
+                    Console.WriteLine("kettő");
+                    break;
+                case 3:
+                    Console.WriteLine("három");
+                    break;
+                case 4:
+                    Console.WriteLine("négy");
+                    break;
+                default:
+                    Console.WriteLine("valami más");
+                    break;
+
+            }
 
         }
     }
