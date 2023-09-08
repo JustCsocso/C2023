a83db83666f1428c628e31bfcb93a4b80ee171c4
Pintér Csongor <pinterc21@ADY-NAGYATAD.SULINET.HU>
2023-09-08 12:31:07 +0200

    s

--- elso/elso/Program.cs
+++ elso/elso/Program.cs
@@ -60,7 +60,76 @@ namespace elso
                     break;
 
             }
+            int b;
+            b = 76;
+            switch (b) 
+            {
+                case 76:
+                    Console.WriteLine("Hetvenhat");
+                    break;
+            }
 
+            double num = 1;
+            while(num<3)
+            {
+                Console.WriteLine(num);
+                num += .1;
+            }
+
+            do
+            {
+                num -= .2;
+                Console.WriteLine(num);
+            }
+            while (num > 2);
+
+            char betu = 'a';
+            while(betu<='z')
+            {
+                Console.Write(betu);
+                betu = (char)(betu + 1);
+
+            }
+            Console.WriteLine();
+
+            int kezd = 32;
+            while (kezd<=255)
+            {
+                Console.Write((char)kezd + "  ");
+                if (kezd % 8 == 7)
+                {
+                    Console.WriteLine();
+                }
+                kezd++;
+               
+            }
+            /*
+            Console.WriteLine();
+            //for ciklus
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);//kiírás
+            }
+            */
+            Console.WriteLine();
+
+
+            int szamol = 0;
+            for (int i = 1; i <= 15; i++)
+            {
+                szamol += i;
+            }
+            Console.WriteLine(szamol);
+
+            for (int i = 9; i >= 0; i--)
+            {
+                Console.WriteLine(i)
+            }
+
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(9 - i);
+            }
         }
     }
 }
