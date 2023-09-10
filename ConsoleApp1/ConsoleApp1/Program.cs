4b02e3784a6e461d8f7ded13f26cc49447abc5c0
Pintér Csongi <98027547+JustCsocso@users.noreply.github.com>
2023-09-10 20:50:15 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i <= 95; i++)
+            {
+                if (i % 5 == 0) 
+                { 
+                    Console.WriteLine(i + " "); 
+                }
+            }
+            
+
+        }
+
+    }
+}
