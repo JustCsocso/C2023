b985b2ecf3b37d25261796c834e7a49702f92272
Pintér Csongi <98027547+JustCsocso@users.noreply.github.com>
2023-09-15 07:13:59 +0200

    Add files via upload

---
+++ haziii/haziii/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hazi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int a = 5; a <= 10; a++)
+            {
+                Console.Write(a + " ");
+
+                Console.Write(a * a + " ");
+                Console.WriteLine();
+            }
+
+
+            string szo = "a";
+            while (szo != "")
+            {
+                Console.WriteLine("Kérek egy szót:");
+                Console.ReadLine();
+            }
+        }
+    }
+}
