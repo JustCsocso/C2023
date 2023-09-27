56896f5f53d2cb301b47374780e37e2606821774
Pintér Csongor <pinterc21@ADY-NAGYATAD.SULINET.HU>
2023-09-27 12:33:49 +0200

    sw

---
+++ gyak0915/gyak0915/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolApp1
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kérek egy szamot: ");
+            int szam=Convert.ToInt32(Console.ReadLine()); 
+            for (int i=0;i<szam; i++)
+            {
+                Console.WriteLine(i*10);
+                Console.WriteLine();
+            }
+
+
+            for(int i = 1; i <= 5; i++)
+            {
+                for (int k=1;k<=5;k++)
+                {
+                    Console.Write(i*k+"\t");
+                }
+                Console.WriteLine();    
+            }
+
+
+            Console.Write("Kérek egy számot: ");
+            int szam2=Convert.ToInt32(Console.ReadLine());
+            for(int i = 1; i <= szam2; i++)
+            {
+                for(int k=1;k<=szam2; k++)
+                {
+                    Console.Write(i*k+"\t");
+                }
+                Console.WriteLine();
+            }
+
+            int szamos = 0;
+            for(int i = 0; i <= 999; i++)
+            {
+                szamos += i;
+                if (szamos <= 999)
+                {
+                    Console.WriteLine(szamos);
+                }
+                else
+                {
+                    Console.WriteLine(i - 1);
+                    break;
+                }
+            }
+            
+        }
+    }
+}
---
+++ gyakszi0927/gyakszi0927/Program.cs
@@ -0,0 +1,111 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Kérj be egy számot, majd írasd ki!
+
+/*
+Console.Write("Kérek egy számot: ");
+int szam=Convert.ToInt32(Console.ReadLine());  
+Console.WriteLine(szam);
+
+//2. Kérj be két számot, írd ki a különbségüket!
+
+Console.Write("Kérek egy számot: ");
+int szam2=Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Kérek egy másik számot: ");
+int szam3=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(szam2-szam3);
+
+//3. Kérj be két tizedestört számot, írd ki a szorzatukat!
+
+Console.Write("Kérek tizedestörtet számot: ");
+double tort1=Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Kérek egy másik  tizedestörtet számot: ");
+double tort2=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(tort1*tort2);
+
+//4. Kérj be két számot, írd ki a nagyobbat!
+
+Console.Write("Kérek egy számot: ");
+int szam4 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Kérek egy másik számot: ");
+int szam5 = Convert.ToInt32(Console.ReadLine());
+
+if (szam4 > szam5)
+{
+    Console.WriteLine(szam4);
+}
+else
+{
+    Console.WriteLine(szam5);
+}
+
+//5. Kérj be két számot, írd ki a tört alakban, ha lehet tört! Pl.: ha a=1 és b=10, akkor "1 / 10 = 0.1"
+
+Console.Write("Kérek egy számot: ");
+int szam6 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Kérek egy másik számot: ");
+double szam7 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(szam6 / szam7);
+
+
+
+//6. Kérj be két számot, írd ki a köztük lévő számokat! ([a,b[)
+Console.Write("Kérek egy számot: ");
+int szam8 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Kérek egy másik számot: ");
+int szam9 = Convert.ToInt32(Console.ReadLine());
+for (int i = szam8 ; i < szam9; i++)
+{
+   
+    Console.WriteLine(i);
+}
+
+
+
+//7. Kérj be két számot, írd ki a köztük lévő számokat, a határokat is! ([a,b])
+
+Console.Write("Kérek egy számot: ");
+int szam10 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Kérek egy másik számot: ");
+int szam11 = Convert.ToInt32(Console.ReadLine());
+for (int i = szam10; i <= szam11; i++)
+{
+
+    Console.WriteLine(i);
+}
+
+
+
+//8. Kérj be két számot, írd ki a köztük lévő számok négyzetét két oszlopban! ([a,b[)
+int szam1 = 0;
+Console.Write( "Kérek egy számot: ");
+int szam12 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Kérek egy másik számot: ");
+int szam13 = Convert.ToInt32(Console.ReadLine());
+for (int i = Math.Min(szam13,szam12)+1; i < Math.Max(szam12,szam13); i++)
+{
+    Console.Write(i * i+" ");
+    szam1 += 1;
+    if (szam1 % 2 == 0)
+    {
+        Console.WriteLine();
+    }
+}
+
+*/
+
+//9. Kérj be három számot, írd ki a legnagyobbat!
+
+Console.Write("Kérek egy számot: ");
+int szam1 = Convert.ToInt32(Console.ReadLine();
+Console.Write("Kérek egy számot: ");
+int szam2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Kérek egy másik számot: ");
+int szam3 = Convert.ToInt32(Console.ReadLine());
