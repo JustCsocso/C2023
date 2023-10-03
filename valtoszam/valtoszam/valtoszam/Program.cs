cdd1e0a4a1717f69c5d6eff67d9c75bf945526d2
Pintér Csongor <pinterc21@ADY-NAGYATAD.SULINET.HU>
2023-10-03 09:39:27 +0200

    d

---
+++ valtoszam/valtoszam/valtoszam/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+bool jo = false;
+
+while (!jo)
+{
+    jo = true;
+
+    Console.Write("Mit váltsak át?(pl. 12 kg): ");
+    string be = Console.ReadLine();
+
+    string[] darab = be.Split(' ');
+    if(darab.Length != 2)
+    {
+        jo=false;
+        continue;
+    }
+
+    double szam = 0;
+    try
+    {
+        szam = Convert.ToDouble(darab[0]);
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Nem jó fumratum! Legyen ilyen: 12,2 kg");
+        jo=false;
+        continue;
+    }
+    Console.WriteLine(szam);
+}
+
+
+
+
