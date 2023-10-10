edf5becbd1cb1758917486d6f7415d41dcb66026
Pintér Csongor <pinterc21@ADY-NAGYATAD.SULINET.HU>
2023-10-10 14:03:46 +0200

    dfw

---
+++ tombok/tombok/tombok/Program.cs
@@ -0,0 +1,34 @@
+﻿void kiir(int[] tomb)
+{
+    foreach (int i in tomb)
+    {
+        Console.WriteLine("{0}.szam", i);
+    }
+}
+
+// See https://aka.ms/new-console-template for more information
+int[]szamok= new int[100];
+
+for (int i = 0; i < szamok.Length; i++)
+{
+    szamok[i] = i+1;
+}
+
+Console.WriteLine(szamok);
+foreach (int i in szamok)
+{
+    Console.WriteLine("{0}.szam",i);
+}
+
+for (int i = 0;i < szamok.Length; i++)
+{
+    szamok[i] = szamok[i] * 2;
+}
+foreach (int i in szamok)
+{
+    Console.WriteLine("{0}.szam", i);
+}
+
+kiir(szamok);
+
+//szorzotabla egyestol 10ig megvalositasa 2 dimenzios tombbel es kiirasa tablazat szeruen
