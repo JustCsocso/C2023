d3cff4ca6444193b88e1fbdfe721217df23433cd
Pintér Csongor <pinterc21@ADY-NAGYATAD.SULINET.HU>
2023-10-13 13:19:07 +0200

    dw

---
+++ random/random1013/random1013/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[]szam=new int[1000];
+for (int i = 0; i < szam.Length; i++)
+{
+    Random rand = new Random();
+    szam[i]= rand.Next(100, 1001);
+}
--- tombok/gyakorlas1013/gyakorlas1013/Program.cs
+++ tombok/gyakorlas1013/gyakorlas1013/Program.cs
@@ -1,7 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-
-
+/*
 Console.Write("Kérek egy szám: ");
 int szam=Convert.ToInt32(Console.ReadLine());
 
@@ -14,10 +12,82 @@ for (int i = 0; i < szoveg.Length; i++)
 }
 
 string max = "";
+//maximum keresés
 for (int i = 0;i < szoveg.Length; i++)
 {
     if (szoveg[i].Length > max.Length)
     {
+        max = szoveg[i];
+    }
+}
+Console.WriteLine(max);
+
+//index keresés
+
+int maxIndex = 0;
+for (int i = 0; i<= szoveg.Length; i++)
+{
+    if (szavak[i] == max)
+    {
+        maxIndex = i;
+    }
+}
+
+//maximum keresés másként
+
+max = "";
+maxIndex = 0;
+for (int i = 0; i < szoveg.Length; i++)
+{
+    if (szoveg[i].Length>max.Length)
+    {
+        max = szoveg[i];
+        maxIndex = i;
+    }
+}
+
+//max keresés 3
+
+maxIndex = 0;
+for (int i = 0;i<szoveg.Length; i++)
+{
+    if (szoveg[i].Length > szoveg[maxIndex].Length)
+    {
+        maxIndex = i;
+    }
+}
+
+*/
+
+string[] szavak = new string[] {"alma","körte",
+                "narancs","mandula",
+                "ananász","banán" };
+
+string[] rovid = new string[szavak.Length];
+string[] hosszu = new string[szavak.Length];
+
+int rovidIndex = 0;
+int hoszzuIndex=0;
+
+for (int i = 0; i < szavak.Length; i++)
+{
+    if (szavak[i].Length >= 5)
+    {
+        rovid[rovidIndex] = szavak[i];
+        rovidIndex++;
+    }
+    else
+    {
+        hosszu[hoszzuIndex++]= szavak[i];
 
     }
+}
+
+for (int i = 0;i < rovid.Length; i++)
+{
+    Console.WriteLine(rovid[i]);
+}
+for(int i = 0;i>hosszu.Length ; i++)
+{
+    Console.WriteLine(hosszu[i]);
 }
