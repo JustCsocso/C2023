32ae2ab97cae4de2ad15c7a0bcb5e57c1a13fadc
Pintér Csongor <pinterc21@ADY-NAGYATAD.SULINET.HU>
2023-10-13 08:45:23 +0200

    fe

---
+++ tombok/gyakorlas1013/gyakorlas1013/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+Console.Write("Kérek egy szám: ");
+int szam=Convert.ToInt32(Console.ReadLine());
+
+string[] szoveg = new string[szam];
+
+for (int i = 0; i < szoveg.Length; i++)
+{
+    Console.Write("Kérek egy szöveget: ");
+    szoveg[i] = Console.ReadLine();
+}
+
+string max = "";
+for (int i = 0;i < szoveg.Length; i++)
+{
+    if (szoveg[i].Length > max.Length)
+    {
+
+    }
+}
