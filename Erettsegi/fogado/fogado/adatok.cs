bfd95c4463e8966c4c8e9235c6cdfc068ee1089d
Pintér Csongor <pinterc21@ADY-NAGYATAD.SULINET.HU>
2024-01-30 11:45:10 +0100

    dw

---
+++ Erettsegi/fogado/fogado/adatok.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fogado
+{
+    internal class adatok
+    {
+        public string nev;
+        public string idopont;
+        public DateTime foglalas;
+        public adatok(string sor)
+        {
+            string[] vag=sor.Split(' ');
+            nev= vag[0]+ " "+vag[1];
+            idopont= vag[2];
+            foglalas = Convert.ToDateTime(vag[3].Replace("-"," "));
+        }
+
+        public override string ToString()
+        {
+            /*
+                Tanár neve: Csorba Ede
+                Foglalt időpont: 16:30
+                Foglalás ideje: 2017.10.28-18:48 
+             */
+            return string.Format("Tanár neve: {0}\r\nFoglalt időpont: {1}\r\nFoglalás ideje: {2} ",nev,idopont,foglalas);
+        }
+    }
+
+}
